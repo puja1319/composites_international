243abcf51e7c47ba999293f1444f38b7ffa040ae
puja1319 <pujadey1319@gmail.com>
2024-11-29 18:18:47 +0530

    minimal changes

--- src/Pages/WeightMonitor.js
+++ src/Pages/WeightMonitor.js
@@ -13,13 +13,13 @@ const WeightMonitor = () => {
                 <div className="main_heading w-100">Gamma Gage Material Weight Monitor</div>
             </div>
             <div className="d-flex justify-content-between margin_main_x mt-5">
-                <div className="d-flex justify-content-between align-items-end" style={{ width: '38%' }}>
+                <div className="d-flex justify-content-between align-items-end w_38">
                     <div className="prod_date_head mb-3">Production Date/ Time :</div>
                     <div className="time_div_font time_div px-4 py-3 bg-white">11/12/2024</div>
                     <div className="time_div_font time_div px-4 py-3 bg-white">06:35:54</div>
                 </div>
-                <div className="d-flex justify-content-between right_tracking_div p-3" style={{ width: '42%' }}>
-                    <div style={{ width: '100%' }} className="me-2">
+                <div className="d-flex justify-content-between right_tracking_div p-3 w_42">
+                    <div className="me-2 w-100">
                         <div className="d-flex justify-content-between align-items-center mb-2">
                             <div className="right_tracking_text w-50">Run Line :</div>
                             <div className="time_div time_div_h_w bg-white d-flex justify-content-center align-items-center right_tracking_value">SMC 1</div>
@@ -29,7 +29,7 @@ const WeightMonitor = () => {
                             <div className="time_div time_div_h_w bg-white d-flex justify-content-center align-items-center right_tracking_value">M369480</div>
                         </div>
                     </div>
-                    <div style={{ width: '100%' }} className="ms-2">
+                    <div className="ms-2 w-100">
                         <div className="d-flex justify-content-between align-items-center mb-2">
                             <div className="right_tracking_text w-50">PackMO :</div>
                             <div className="time_div time_div_h_w bg-white d-flex justify-content-center align-items-center right_tracking_value">M369470</div>
